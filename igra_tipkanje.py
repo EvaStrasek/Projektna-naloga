573386a4292846cad46bb2626bb73c4d221bc653
EvaStrasek <111704422+EvaStrasek@users.noreply.github.com>
2022-10-12 00:05:04 +0200

    naredila novo okno in ga centrirala

--- igra_tipkanje.py
+++ igra_tipkanje.py
@@ -32,8 +32,6 @@ def casovnik(cas):
 def besede_na_minuto(st_besed,cas):
     return (st_besed / cas) * 60
 
-def cas():
-    pass
 
 
 
--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,1 +1,19 @@
-import turtle
+from tkinter import *
+
+okno = Tk()
+okno.title("Hitrostno tipkanje")
+okno.configure(bg = "dark green")
+okno.configure(width = 800, height = 600)
+
+#centrira okno
+winWidth = okno.winfo_reqwidth()
+winwHeight = okno.winfo_reqheight()
+posRight = int(okno.winfo_screenwidth() / 2 - winWidth / 2)
+posDown = int(okno.winfo_screenheight() / 2 - winwHeight / 2)
+okno.geometry("+{}+{}".format(posRight, posDown))
+#
+
+#gumb za start
+
+
+okno.mainloop()
