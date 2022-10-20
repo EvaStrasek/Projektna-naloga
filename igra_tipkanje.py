709137c8971b98be0ea53ddc65e958aaf5b1dacd
EvaStrasek <111704422+EvaStrasek@users.noreply.github.com>
2022-10-20 16:41:11 +0200

    preimenovala besedila, uredila igra.tpl

--- igra_tipkanje.py
+++ igra_tipkanje.py
@@ -2,16 +2,19 @@ from encodings import utf_8
 import random
 import datetime
 import json
+from tkinter.tix import Tree
 
 
 class Igra():
     
-    def __init__(self,besedilo):
-        self.besedilo = besedilo
+    def __init__(self):
+        self.trenutno_besedilo = ''
         self.zacetni_cas = ''
         self.koncni_cas = ''
+        self.polje_povedi = []
+        self.trenutni_indeks = 0
 
-    def preberi_datoteko(datoteka):
+    def preberi_datoteko(self,datoteka):
         with open(datoteka,'r',encoding='utf-8') as f:
             besedilo = []
             vrstice = json.load(f)
@@ -23,14 +26,17 @@ class Igra():
     def nastavi_zacetni_cas(self):
         self.zacetni_cas = datetime.datetime.now()
 
-    def izbor_random_crke():
+    def izbor_random_crke(self):
         crke = 'ABCČDEFGHIJKLMNOPQRSŠTUVWXYZŽ'
-        crka = random.choice(crke)
-        return crka
+        i = 0
+        for i in range(20):
+            self.polje_povedi.append(random.choice(crke))
+        self.trenutno_besedilo = self.polje_povedi[0]
     
-    def pravilnost_besed(self,vnesena_beseda):
-        if self.besedilo == vnesena_beseda:
+    def pravilnost_besed(self,vneseno_besedilo):
+        if self.trenutno_besedilo == vneseno_besedilo:
             self.koncni_cas = datetime.datetime.now()
+            self.naslednje_besedilo()
             return True
         else: return False
 
@@ -39,30 +45,33 @@ class Igra():
        # stetje napak preko definicije pravilnost_besed
         return st_napak
 
-
-class Tipkanje():
-    def __init__(self):
-        self.igre = []
-        self.trenutna_igra = 0 
-        self.st_besed_v_povedi = 0
-        self.st_vrstic = 0
-    
-    def nova_igra(self,st_besed_v_povedi,st_vrstic):
-        self.igre = []
-        self.trenutna_igra = 0
-        self.st_besed_v_povedi = st_besed_v_povedi
-        self.st_vrstic = st_vrstic
-
+    def pripravi_besedilo(self, datoteka):
+        besedilo = self.preberi_datoteko(datoteka)
         i = 0
-        while i < st_vrstic:
-            igra = Igra.nova_igra(st_besed_v_povedi)
-            self.igre.append(igra)
-            i += 1
-        return self.trenutna_igra
 
-besedilo1 = Igra.preberi_datoteko("besedilo1.json")
-besedilo2 = Igra.preberi_datoteko("besedilo2.json")
-besedilo3 = Igra.preberi_datoteko("besedilo3.json")
+        for i in range(len(besedilo)):
+            self.polje_povedi.append(besedilo[i])
+
+        self.trenutno_besedilo = self.polje_povedi[0]
+
+    def nova_igra(self, tezavnost):
+        self.polje_povedi = []
+        self.trenutni_indeks = 0
+        self.trenutno_besedilo = ''
+        
+        if tezavnost == 'zelo lahko':
+            self.izbor_random_crke()
+        elif tezavnost == 'lahko':
+            self.pripravi_besedilo('./besedila/besedilo-lahko.json')
+        elif tezavnost == 'srednje':
+            self.pripravi_besedilo('./besedila/besedilo-srednje.json')
+        elif tezavnost == 'težko':
+            self.pripravi_besedilo('./besedila/besedilo-tezko.json')
+
+    def naslednje_besedilo(self):
+        self.trenutni_indeks += 1
+        self.trenutno_besedilo = self.polje_povedi[self.trenutni_indeks]
+
 
     
 def besede_na_minuto(st_besed,cas):
--- views/zacetna_stran.tpl
+++ views/zacetna_stran.tpl
@@ -3,16 +3,18 @@
 % rebase('osnovna_stran.tpl')
 <nav class="level">
   <div>
-    <div>Izberite si poljubno težavnost igre nato pa za začetek igre kliknite gumb Start</div>
-    <div>
+    <form action="/nova-igra" method="POST">
+      <div>Izberite si poljubno težavnost igre nato pa za začetek igre kliknite gumb Start</div>
+      <div>
           Težavnost
           <select name="tezavnost" id="tezavnost">
             %for tezavnost in tezavnosti:
             <option value="{{tezavnost}}">{{tezavnost}}</option>
             %end
-    </div>
-    <div>
-      <input class="button-is-info" type="submit" value="START"></input>
-    </div>
+      </div>
+      <div>
+        <input class="button-is-info" type="submit" value="START"></input>
+      </div>
+    </form>
   </div>
 </nav>
