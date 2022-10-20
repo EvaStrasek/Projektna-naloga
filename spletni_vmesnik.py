bf11c071ba3d94d46b7c7073ff1fc5d22d8dde15
EvaStrasek <111704422+EvaStrasek@users.noreply.github.com>
2022-10-20 16:42:56 +0200

    uredila igra.tpl

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,28 +1,35 @@
 import bottle
-from igra_tipkanje import Igra,Tipkanje
+from igra_tipkanje import Igra
 
-igra_tipkanje = Tipkanje()
+igra_tipkanje = Igra()
 
 @bottle.get('/')
 def zacetna_stran():
     tezavnosti = ["zelo lahko","lahko","srednje","težko"]
     return bottle.template('zacetna_stran.tpl',tezavnosti = tezavnosti)
 
-@bottle.post('/nova_igra')
+@bottle.post('/nova-igra')
 def nova_igra():
     tezavnost = bottle.request.forms.getunicode('tezavnost')
-    if tezavnost == "zelo lahko":
-        #"izbira_random_crke * 20"
-        pass
-    elif tezavnost == "lahko":
-        # besedilo3.json
-        pass
-    elif tezavnost == "srednje":
-        # besedilo2.json
-        pass
-    elif tezavnost == "težko":
-        # besedilo1.json
-        pass
+    igra_tipkanje.nova_igra(tezavnost)
+
+    bottle.redirect('/igra')
+
+@bottle.get('/igra')
+def igra():
+    trenutno_besedilo = igra_tipkanje.trenutno_besedilo
+
+
+    return bottle.template('igra.tpl', trenutno_besedilo = trenutno_besedilo)
+
+@bottle.post('/preveri-besedilo')
+def preveri_besedilo():
+    besedilo = bottle.request.forms.getunicode('besedilo')
+    igra_tipkanje.pravilnost_besed(besedilo)
+    bottle.redirect('/igra')
+
+
+
 
 
 bottle.run(debug = True,reload = True)
--- views/igra.tpl
+++ views/igra.tpl
@@ -1,6 +1,17 @@
 % import igra_tipkanje
-% rebase(osnovna_stran.tpl)
+% rebase('osnovna_stran.tpl')
 
-<nav>
-  <div></div>
+<nav class = "level">
+    <div class = "box">
+        <div>
+            Vpišite naslednje besedilo:
+        </div>
+        <div class = "panel-block">
+            <h1> {{trenutno_besedilo}}</h1>
+        </div>
+        <form action = "/preveri-besedilo" method = "POST">
+            Vnos: <input type = "text" class = "input is-small is rounded" autofocus name ="besedilo">
+            <input type = "submit" class = "button is-info" value = "Vpiši">
+        </form>
+    </div>
 </nav>
