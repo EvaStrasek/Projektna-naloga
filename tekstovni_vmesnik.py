a14a9610331f0875679a9bf9dc9a0e4d6c505c32
EvaStrasek <111704422+EvaStrasek@users.noreply.github.com>
2022-10-25 00:58:11 +0200

    napisala tekstovni vmesnik za uporabo

--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -1,18 +1,61 @@
-import igra_tipkanje
+from igra_tipkanje import Igra
 
-def start():
-    navodila = 'Na voljo imaš {cas}s da natipkaš čim več besed. Za začetek igre klikni gumb Start.'  
-    print(navodila)
+igra = Igra()
+
+def start(): 
+    print('Pozdravljeni v igri Hitrostno tipkanje. Za začetek igre vpišite številko za izbrano težavnost.')
+    print('1 - zelo lahko')
+    print('2 - lahko')
+    print('3 - srednje')
+    print('4 - težko')
+
+def izberi_tezavnost():
+    tezavnost = input('Številka: ')
+    if tezavnost == '1':
+        return 'zelo lahko'
+    elif tezavnost == '2':
+        return 'lahko'
+    elif tezavnost == '3':
+        return 'srednje'
+    elif tezavnost == '4':
+        return 'težko'
+    else:
+        print('Neveljaven vnos')
+        izberi_tezavnost()
+    
+
+def nova_igra():
+    tezavnost = izberi_tezavnost()
+    igra.nova_igra(tezavnost)
+    while igra.trenutni_indeks != len(igra.polje_povedi) - 1:
+        print(igra.trenutno_besedilo + " (" + str(igra.trenutni_indeks + 1) + "/" + str(len(igra.polje_povedi)-1) + ")")
+        vnos = input('> ')
+        if not igra.pravilnost_besed(vnos):
+            print('Napačen vnos')
+    konec()
+
 
 def konec():
-    print('Konec igre. Čas se je iztekel!')
-
-def statistika(cas,st_besed):
-    #st_napak = igra_tipkanje.Igra.stetje_napak(besede)
-    wpm = igra_tipkanje.Igra.besede_na_minuto(st_besed,cas)
-    if wpm >= 40:
-        print(f"Bravo! \nTvoje povprečno število natipkanih besed na minuto(wpm) je {wpm}. \nPovprečen človek ima 40wpm")
+    print('Konec igre!')
+    st_napak = igra.st_napak
+    cas = round((igra.koncni_cas - igra.zacetni_cas).total_seconds(),2)
+    wpm = igra.besede_na_minuto(cas)
+    print('Čas: ' + str(cas))
+    print('Število napak: ' + str(st_napak))
+    print('Povprečno število napisanih besed na minuto (wpm): ' + str(wpm))
+    
+def nadaljevanje():
+    print("Če hočete igro ponovno igrati vnesite DA, če hočete igro zaključiti vnesite NE")
+    vnos = input('> ')
+    if vnos.upper() == 'DA':
+        tekstovni_vmesnik()
     else:
-        print(f"Tvoje povprečno število natipkanih besed na minuto(wpm) je {wpm}. \nPovprečen človek ima 40wpm")
+        print('Z igro ste zaljučili.')
 
 
+def tekstovni_vmesnik():
+    start()
+    nova_igra()
+    nadaljevanje()
+
+#tekstovni_vmesnik()
